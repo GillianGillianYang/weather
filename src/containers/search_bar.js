ca53ca636a4a5f0fc03a866573e78bab953102e0
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-08-02 17:22:06 +0800

    53 controlled component and blinding context
    53 controlled component and blinding context
    
    this.onInputChange = this.onInputChange.bind(this);

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {term:''};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event){ // Event -> vanilla.js
+        console.log(event.target.value);
+       // console.log("search compoment :",this);this -> undefined //mystery context
+        this.setState({term:event.target.value}); //"this" will not be this search component ; it will refer other . 
+        
+    }
+
+    //onChange={this.onInputChange} //flat arrow function 
+    //it is a  callbakc function // but the "this will be mystery context "
+
     render(){
         return(
            <form className='input-group'>
-               <input />
+               <input
+                    placeholder='Get a 5-day forecast in your favorite city' 
+                    className='form-control' 
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                    />
                <span className='input-group-btn'>
                    <button type='submit' className="btn btn-secondary">Submit</button>
                </span>    
