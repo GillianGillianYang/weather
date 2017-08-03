12315b7750acaa59c943c5a0fc10d7b0157ca4e6
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-08-03 17:52:39 +0800

    57. Ajax Request with Axios
    57Ajax Request with Axios
    
    npm install —save axios
    // instead of using jquery >> $.get(url);
    //   we use axis.get(url); >> return Promise
    
    axios : Promise based HTTP client for the browser and node.js
    https://github.com/mzabriskie/axios
    
    we only change application states through actions or reducers
    http://api.openweathermap.org/data/2.5/forecast?q=Milan,IT&appid=96ceeb8
    40628323adbf
    
    IT
    Milan
    Roma
    Verona
    Venezia
    Firenze

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
-const APIKEY ='96ceeb840628323adbfcbf43ff6d3db8';
+import axios from 'axios';
+
+const API_KEY ='96ceeb840628323adbfcbf43ff6d3db8';
+const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER ='FETCH_WEATHER'; 
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},IT`;
+    //$.get(url); --> axios
+    const request = axios.get(url); // return Promise
+
+    return(
+        {
+            type : FETCH_WEATHER,
+            payload: request
+        }
+    );
+}
