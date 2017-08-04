b2957776a138fdba8bb6156e8ddeb1140120f672
MrRabit <Gillian@GilliantekiMacBook-Air-2.local>
2017-08-04 13:11:53 +0800

    58.Redux -Promise in Practice
    58Redux -Promise in Practice
    
    container needs to call action creator (redux) >>connect method (
    react-redux lib )
    // check localhost:8080
    
    export default connect(nulll, mapDispatchToProps)(SearchBar);
    
    before : we connect action to props or  connect state to props ;
    here the mapDispatchToProps is supposed to map dispatch to props of our
    container , it always goes in as 2 argument ;
    this step , null means it does’t care about state at all;
    //!!!!!
    
    in chrome develop tool  > network > XHR > Preview  將會show  出API 回傳的son
    資料
    
    react-redux的 connect 用法 :
    http://taobaofed.org/blog/2016/08/18/react-redux-connect/
    
    connect
    https://github.com/reactjs/react-redux/blob/master/docs/api.md#connectma
    pstatetoprops-mapdispatchtoprops-mergeprops-options
    
    redux https://github.com/reactjs/redux/blob/master/docs/README.md

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,22 +1,31 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component{
+
+//export default
+ class SearchBar extends Component{
     constructor(props){
         super(props)
 
         this.state = {term:''};
         this.onInputChange = this.onInputChange.bind(this); 
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event){ // Event -> vanilla.js
         console.log(event.target.value);
        // console.log("search compoment :",this);this -> undefined //mystery context
         this.setState({term:event.target.value}); //"this" will not be this search component ; it will refer other . 
-        
     }
+
     onFormSubmit(event){
         event.preventDefault();
         //we need to go and fetch weather data
+        this.props.fetchWeather(this.state.term); // ?? why  this.props ??? 
+        // error : Cannot read property 'props' of null -> bind this 
+        this.setState({term:''});
     }
 
     //onChange={this.onInputChange} //flat arrow function 
@@ -37,5 +46,11 @@ export default class SearchBar extends Component{
            </form>   
         );
     }
-
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({fetchWeather}, dispatch); 
+    // bind action creators(fetchWeather) with dispatch, make sure action will flows through middleware and reducers(in redux)
+}
+
+export default connect(null,mapDispatchToProps)(SearchBar); //!!!!!
